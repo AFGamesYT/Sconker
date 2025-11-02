82422c3382b228e6fddf84d2192cab95dc1475c5
Artjoms <a.f.gamesyt123@gmail.com>
2025-11-03 01:16:52 +0200

    Conker class, and first commit

--- main.py
+++ main.py
@@ -5,5 +5,3 @@ class Conker:
         self.RARITIES = {"common": 80, "rare": 15, "ultra": 5}
 
         self.rarity = choices(list(self.RARITIES.keys()), weights=list(self.RARITIES.values()))[0]
-
-
