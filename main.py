d63c14679becc52bcda67e0589595cc6618e7798
Artjoms <a.f.gamesyt123@gmail.com>
2025-11-03 01:14:02 +0200

    Conker class, and first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from random import choices
+
+class Conker:
+    def __init__(self):
+        self.RARITIES = {"common": 80, "rare": 15, "ultra": 5}
+
+        self.rarity = choices(list(self.RARITIES.keys()), weights=list(self.RARITIES.values()))[0]
+
+
