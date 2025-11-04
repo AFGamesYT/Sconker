ef22bf1d9bf46ca24bafea6a171b848baf4ec468
Artjoms <a.f.gamesyt123@gmail.com>
2025-11-04 20:32:31 +0200

    added the very base of rarities

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from random import choices
 
+class Common:
+    def __init__(self):
+        self.chance = 80
+        self.multiplier = 1
+
+common = Common()
+
+class Rare:
+    def __init__(self):
+        self.chance = 15
+        self.multiplier = 1.5
+
+rare = Rare()
+
+class Ultra:
+    def __init__(self):
+        self.chance = 5
+        self.multiplier = 7
+
+ultra = Ultra()
+
 class Conker:
     def __init__(self):
-        self.RARITIES = {"common": 80, "rare": 15, "ultra": 5}
+        self.RARITIES = {"common": common.chance, "rare": rare.chance, "ultra": ultra.chance}
 
-        self.rarity = choices(list(self.RARITIES.keys()), weights=list(self.RARITIES.values()))[0]
+        self.rarity = choices(
+            list(self.RARITIES.keys()),
+            weights=list(self.RARITIES.values())
+        )[0]
+
