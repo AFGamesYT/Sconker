6410becf4780f3c7abec581f5a6bf13857ef62da
Artjoms <a.f.gamesyt123@gmail.com>
2025-12-05 14:48:52 +0200

    Add growing animation, Add conker_grow.mp3, Get rid of abundant classes

--- main.py
+++ main.py
@@ -6,47 +6,24 @@ import pygame
 from math import pi, sqrt, sin, cos
 from random import choices, uniform, random, randint
 
-class Common:
-    def __init__(self):
-        self.chance = 80
-        self.multiplier = 1
+from playsound3 import playsound
 
-common = Common()
+class ConkerModifier:
+    def __init__(self, name, mod_type, chance, multiplier):
+        self.name = name
+        self.type = mod_type
+        self.chance = chance
+        self.multiplier = multiplier
 
-class Rare:
-    def __init__(self):
-        self.chance = 15
-        self.multiplier = 1.5
 
-rare = Rare()
+common = ConkerModifier("common", "rarity", 80, 1)
+rare = ConkerModifier("rare", "rarity", 15, 1.5)
+ultra = ConkerModifier("ultra", "rarity", 5, 2.5)
 
-class Ultra:
-    def __init__(self):
-        self.chance = 5
-        self.multiplier = 2.5
+single = ConkerModifier("single", "amount", 80, 1)
+double = ConkerModifier("double", "amount", 15, 2)
+triple = ConkerModifier("triple", "amount", 5, 3)
 
-ultra = Ultra()
-
-class Single:
-    def __init__(self):
-        self.chance = 80
-        self.multiplier = 1
-
-single = Single()
-
-class Double:
-    def __init__(self):
-        self.chance = 15
-        self.multiplier = 2
-
-double = Double()
-
-class Triple:
-    def __init__(self):
-        self.chance = 5
-        self.multiplier = 3
-
-triple = Triple()
 
 RARITIES = {"common": common.chance, "rare": rare.chance, "ultra": ultra.chance}
 MULTIPLIERS = {"single": single.chance, "double": double.chance, "triple": triple.chance}
@@ -54,7 +31,7 @@ MULTIPLIERS = {"single": single.chance, "double": double.chance, "triple": tripl
 class Tree:
     def __init__(self):
         self.growth_time = 5
-        self.max_tree_conker_amount = 10
+        self.max_tree_conker_amount = 17
         self.new_conker_appear_time = 1
 
         self.conkers_on_tree: list[Conker] = []
@@ -75,24 +52,31 @@ class Conker:
 
 
         angle = uniform(0, 2 * pi)
-        radius = 0.42 * HEIGHT / 2
+        radius = 0.53 * HEIGHT / 2
         r = radius * sqrt(random())
         x = r * cos(angle)
         y = r * sin(angle)
 
-        self.position = (WIDTH/2 + x, HEIGHT/3 + y)
+        self.position = (WIDTH/2 + x, HEIGHT*2/5 + y)
 
         self.size = randint(60, 75)  # TODO: (later) upgrade: "Make the conkers smaller. Makes more fit on the tree"
+        self.actual_size = 0
         # self.rotation = randint(0, 359)
         self.rotation = 0  # TODO: looks weird when rotated, because of the shadow. change this
 
         self.surface = pygame.transform.rotate(pygame.transform.scale(conker_t, (self.size, self.size)), self.rotation)
-        self.surface_pos = (self.position[0]-self.size/2, self.position[1]-self.size/2)
+        self.surface_pos = [self.position[0]-self.actual_size/2, self.position[1]-self.actual_size/2]
 
         self.growing = True
+        self.grow_time = tree.growth_time + randint(0, 2)
+        self.spawn_time = pygame.time.get_ticks()
+
+    def update_surface_pos(self):
+        self.surface_pos = [self.position[0]-self.actual_size/2, self.position[1]-self.actual_size/2]
 
     def __str__(self):
         return f"Conker: rarity={self.rarity}, multiplier={self.multiplier}, growing={self.growing}."
+
 
 def spawn_conker(v=False):
     if not len(tree.conkers_on_tree) < tree.max_tree_conker_amount:  # check if there is space on the tree to grow
@@ -133,7 +117,9 @@ def spawn_conker(v=False):
 
     tree.conkers_on_tree.append(new_conker)
 
-    sleep(tree.growth_time+randint(0, 3))  # TODO: think of a smarter way to vary the conker growth time
+
+    sleep(new_conker.grow_time)
+    playsound("sounds/conker_grow.mp3", block=False)
     if v: print("Conker finished growing.")
 
     new_conker.growing = False
@@ -154,13 +140,13 @@ def update():  # function to update variables and other things.
 
 pygame.init()
 
-
 # textures
 background_day_t = pygame.image.load("images/background_day.png")
 sun_t = pygame.image.load("images/sun.png")
 tree_ground_t = pygame.image.load("images/tree_ground.png")
 shadow_t = pygame.image.load("images/shadow.png")
 conker_t = pygame.image.load("images/conker.png")
+
 
 
 # resolution
@@ -181,7 +167,6 @@ fullscreen = True
 while run:
     spawn_conker_thread = Thread(target=lambda: spawn_conker(), daemon=True)
 
-
     bg = screen.blit(pygame.transform.scale(
         background_day_t, (WIDTH, HEIGHT)), (0, 0))  # TODO: replace with day/night cycle
 
@@ -197,14 +182,39 @@ while run:
 
     for conker in tree.conkers_on_tree:
         # shadow for the conker to see better
+        ticks = pygame.time.get_ticks()
+
+        if conker.growing:
+            grown_percent = (ticks - conker.spawn_time) / (conker.grow_time * 1000)
+        else:
+            grown_percent = 1
+        
+        shadow_size = conker.size*1.7*grown_percent
+        
         screen.blit(pygame.transform.scale(
-                shadow_t, (conker.size*1.7, conker.size*1.7)
-            ), (conker.position[0]-conker.size*1.7/2, conker.position[1]-conker.size*1.7/2))
+                shadow_t, (shadow_size, shadow_size)
+            ), (conker.position[0]-shadow_size/2, conker.position[1]-shadow_size/2))
+
 
         # the conker
-        screen.blit(conker.surface, conker.surface_pos)
+        if conker.growing:
+            conker.actual_size = conker.size*grown_percent
+            conker.update_surface_pos()
 
-        # print(f"drawing a conker on {conker.position}, with size {conker.size} and rotation {conker.rotation}")
+            # print(f"target size: {conker.size} | grown percent: {grown_percent*100}% | ticks since conker spawned: {ticks-conker.spawn_time} | conker size: {conker.size*grown_percent}")
+
+            # time passed since the conker spawned / milliseconds per 1 growth step
+
+            screen.blit(pygame.transform.rotate(
+                pygame.transform.scale(conker_t, (
+
+                    conker.actual_size,
+                    conker.actual_size)
+
+                ), conker.rotation), conker.surface_pos)
+        else:
+            screen.blit(conker.surface, conker.surface_pos)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
